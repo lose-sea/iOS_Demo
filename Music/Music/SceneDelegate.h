8a556128c69115a8eac535d8b2a7923805443db8
lose_sea <2031938936@qq.com>
2026-05-11 22:46:53 +0800

    test

---
+++ Music/Music/SceneDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.h
+//  Music
+//
+//  Created by lose_sea on 2026/4/29.
+//
+
+#import <UIKit/UIKit.h>
+#import "VCFirst.h"
+#import "ViewController.h"
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ zara/zara/Homepage.h
@@ -0,0 +1,17 @@
+//
+//  homepage.h
+//  zara
+//
+//  Created by lose_sea on 2026/5/5.
+//
+
+#import <UIKit/UIKit.h>
+#import <Masonry/Masonry.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Homepage : UIViewController <UIScrollViewDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
