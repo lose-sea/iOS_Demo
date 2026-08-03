d7fe6b9e832ce6122a9809a7a1e7dcdfb88b13d9
lose_sea <238879830+lose-sea@users.noreply.github.com>
2026-08-03 19:03:20 +0800

    test

--- 网络请求/网络请求/ViewController.m
+++ 网络请求/网络请求/ViewController.m
@@ -6,6 +6,8 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking/AFNetworking.h>
+
 
 @interface ViewController ()
 
@@ -15,14 +17,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view setBackgroundColor: [UIColor systemCyanColor]];
     // Do any additional setup after loading the view.
+//    [self createURL];
+
+    [self createAFNetworking];
 }
 
 
 - (void) createURL {
-    
     // 创建请求地址
-    NSString* str = @"https://your-api.com/login?username=testuser&password=123456";
+    NSString* str = @"https://api.weatherapi.com/v1/forecast.json?key=3557d02150d248e6b0735224252907&q=西安&days=1";
     NSURL* url = [NSURL URLWithString: str];
     
     // 创建请求类
@@ -40,7 +45,6 @@
                                                           delegate: nil
                                                      delegateQueue: [NSOperationQueue mainQueue]];
 
-    
     // 创建任务
         // 通过URL 进行创建
 //    NSURLSessionDataTask* task = [session dataTaskWithURL: url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
@@ -49,8 +53,48 @@
     
         // 通过 Request 创建
     NSURLSessionDataTask* task = [session dataTaskWithRequest: request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        
+        if (error) {
+            NSLog(@"请求失败: %@", error);
+        } else {
+            NSError* jsonError = nil;
+            // 调用苹果原生 API 将服务器返回的二进制数据 (NSData) 转换为 Objective-C对象
+            // 传入错误指针的地址, 如果二进制数据不是合法的 JSON 格式（比如缺了一个引号，或者是 HTML 报错页），jsonError 就会被赋值为一个包含具体错误原因的对象，而方法本身会返回 nil。
+            id dict = [NSJSONSerialization JSONObjectWithData: data options:0 error: &jsonError];
+            if (jsonError) {
+                NSLog(@"JSON 解析失败");
+            } else {
+                NSLog(@"%@", dict);
+            }
+        }
     }];
     
+    [task resume];
+}
+
+
+// 使用AFNetworking创建网路请求
+- (void) createAFNetworking {
+    // 创建管理器
+    AFHTTPSessionManager* manager = [AFHTTPSessionManager manager];
+    
+    // 设置超时时间 对应原生的 request.timeoutInterval
+    manager.requestSerializer.timeoutInterval = 15;
+    
+    // 设置响应格式 默认为JSON
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+
+    // 拆解 URL 参数
+    NSDictionary* parameters = @{@"key": @"3557d02150d248e6b0735224252907", @"q": @"西安", @"days": @"1"};
+    
+    //发起GET请求
+    [manager GET: @"https://api.weatherapi.com/v1/forecast.json"
+      parameters: parameters
+         headers: nil
+        progress:nil
+         success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"%@", responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"请求失败");
+    }];
 }
 @end
