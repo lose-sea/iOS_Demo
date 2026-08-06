ffa4210efae7e61b81d1d67c95a8ff566cec5351
lose_sea <238879830+lose-sea@users.noreply.github.com>
2026-08-06 15:50:10 +0800

    test

--- UIImagePickerController/UIImagePickerController/ViewController.m
+++ UIImagePickerController/UIImagePickerController/ViewController.m
@@ -1,14 +1,14 @@
 //
 //  ViewController.m
-//  UIImagePickerController
+//  UIImagePickController
 //
 //  Created by lose_sea on 2026/8/6.
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import <Masonry/Masonry.h>
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@property (nonatomic, strong) UIImageView* imageView;
 @end
 
 @implementation ViewController
@@ -16,6 +16,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIButton* button = [UIButton buttonWithType: UIButtonTypeSystem];
+    [self.view addSubview: button];
+    [button mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.view).offset(50);
+        make.left.mas_equalTo(self.view).offset(50);
+        make.width.height.mas_equalTo(100);
+    }];
+    button.backgroundColor = [UIColor systemRedColor];
+    [button setTitle: @"选择图片" forState: UIControlStateNormal];
+    [button addTarget: self action: @selector(pressButton) forControlEvents: UIControlEventTouchUpInside];
+    
+    // 显示在 ImageView 上
+    UIImageView* imageView = [[UIImageView alloc] init];
+    [self.view addSubview: imageView];
+    [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.mas_equalTo(self.view);
+        make.width.height.mas_equalTo(200);
+    }];
+    imageView.backgroundColor = [UIColor systemCyanColor];
+    self.imageView = imageView;
+}
+
+- (void) pressButton {
+    UIImagePickerController* imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.delegate = self;
+    imagePickerController.allowsEditing = YES;
+    //  选择图片来源: 相册
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    
+    // 展示UIiamgePickerController
+    [self presentViewController: imagePickerController animated: YES completion: nil];
+}
+
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    
+//    UIImage* image = info[UIImagePickerControllerOriginalImage];
+    
+    // 获取编辑后的图片（如果 allowsEditing = YES）
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    self.imageView.image = editedImage;
+    
+    
+    [picker dismissViewControllerAnimated: YES completion: nil];
 }
 
 
