265b56712c6f8c9d8232bd42329f97b0bceb3b18
lose_sea <238879830+lose-sea@users.noreply.github.com>
2026-08-13 15:14:46 +0800

    test

--- MVP模式/Pods/Target Support Files/Pods-MVP模式/Pods-MVP模式-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MVP__ : NSObject
-@end
-@implementation PodsDummy_Pods_MVP__
-@end
--- UIImagePickerController/Pods/Target Support Files/Pods-UIImagePickerController/Pods-UIImagePickerController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UIImagePickerController : NSObject
-@end
-@implementation PodsDummy_Pods_UIImagePickerController
-@end
