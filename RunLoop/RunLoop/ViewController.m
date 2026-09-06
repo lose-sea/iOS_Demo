e844c1d162e9ff6b7036d0c3e296fec1e4fadc73
lose_sea <238879830+lose-sea@users.noreply.github.com>
2026-09-06 21:22:21 +0800

    Update ViewController.m

--- RunLoop/RunLoop/ViewController.m
+++ RunLoop/RunLoop/ViewController.m
@@ -9,6 +9,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, assign) NSInteger timeCount;
+
 @end
 
 @implementation ViewController
@@ -17,8 +19,42 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    NSRunLoop* runLoop = [NSRunLoop mainRunLoop]; 
+//    NSRunLoop* runLoop = [NSRunLoop mainRunLoop]; 
     
+//    NSTimer* timer = [NSTimer scheduledTimerWithTimeInterval: 1 target: self selector: @selector(sayHello) userInfo: nil repeats: YES];
+    
+//    NSTimer* timer = [NSTimer scheduledTimerWithTimeInterval: 1
+//                                                      target: self
+//                                                    selector: @selector(sayHello)
+//                                                    userInfo: nil
+//                                                     repeats: YES];
+    
+//    for (NSInteger i = 0; i < 4; i++) {
+//        NSLog(@"xinyan");
+//        sleep(2);
+//    }
+//    NSInteger num = 0;
+//    scanf("%ld", &num);
+//    printf("num = %ld", num);
+    
+    NSLog(@"viewDidLoad 开始执行");
+    
+    self.timeCount = 0;
+    NSTimer* timer = [NSTimer scheduledTimerWithTimeInterval: 1
+                                                      target: self
+                                                    selector: @selector(sayHello)
+                                                    userInfo: nil
+                                                     repeats: YES];
+    
+//    NSRunLoop* runLoop = [NSRunLoop currentRunLoop];
+//    NSLog(@"%@", runLoop);
+    
+    
+    
+}
+
+- (void) sayHello {
+    printf("hello world");
 }
 
 
